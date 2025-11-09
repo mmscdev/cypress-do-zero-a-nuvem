50a1ad0b5e3720e4a6943f79d43f9cde4af5500d
mmerlinscf <mayara.merlim@ventureslab.io>
2025-11-09 13:44:46 -0300

    ajustar projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'r81t1a',
+  projectId: 'um6382',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}
